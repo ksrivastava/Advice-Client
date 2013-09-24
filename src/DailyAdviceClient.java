859d1a69c52d173faa3b0e143bf1bd4ab20162f5
Kos Srivastava <kos.srivastava@gmail.com>
2013-09-24 13:03:29 -0400

    Client for Daily Advice

---
+++ src/DailyAdviceClient.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+
+public class DailyAdviceClient {
+
+	/**
+	 * @param args
+	 */
+	
+	public void go() {
+		try {
+			
+			Socket s = new Socket("127.0.0.1", 4242);
+			InputStreamReader streamReader = new InputStreamReader(s.getInputStream());
+			BufferedReader reader = new BufferedReader(streamReader);
+			
+			String advice = reader.readLine();
+			System.out.println("Today you should: " + advice);
+			reader.close();
+			
+		}
+		
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DailyAdviceClient client = new DailyAdviceClient();
+		client.go();
+	}
+
+}
